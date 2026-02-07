6c412f009ee053fb61ffbd9789af65f92dde7dd6
rasiharunart1 <2211102080@ittelkom-pwt.ac.id>
2026-02-07 21:18:59 +0700

    new

--- routes/dashboard.route.js
+++
@@ -1,7 +0,0 @@
-const {dashboardAtas,} = require('../controllers/dashboard.controller');
-const router = require('express').Router();
-const verifyToken = require('../libs/verifyToken');
-
-router.get('/atas', verifyToken, dashboardAtas);
-
-module.exports = router;
--- routes/report.route.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const { generateReport } = require("../controllers/report.controller");
-
-router.get("/generate", generateReport);
-
-module.exports = router;
